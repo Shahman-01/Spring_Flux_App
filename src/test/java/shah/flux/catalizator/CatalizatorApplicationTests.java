2e5561ca8b87a70dd8916d02bf5d4b50ba8b8ade
Shahman Magomadov <magomadovshahman@gmail.com>
2023-02-22 16:50:23 +0300

    get started

---
+++ src/test/java/shah/flux/catalizator/CatalizatorApplicationTests.java
@@ -0,0 +1,13 @@
+package shah.flux.catalizator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalizatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
